32751f39da8956e6bbd5c6ae058fe5d7c1972e02
asus-rog <prvnpa4@gmail.com>
2025-01-08 12:40:36 +0900

    Program updated with def function and alternate to unsplash img link

--- tam-qt.py
+++ tam-qt.py
@@ -4,7 +4,7 @@ import random
 import textwrap
 
 # create image object with random background
-img_url = requests.get('https://source.unsplash.com/random/800x800?nature,minimal').url
+img_url = requests.get('https://picsum.photos/800/800').url
 img = Image.open(requests.get(img_url, stream=True).raw)
     
 # function to draw text on image
@@ -13,8 +13,8 @@ def draw_text(text, subtext):
     width, height = img.size
     font_size = int(width / 25)
     subtext_font_size = int(font_size / 1.5)
-	
-    image_bg = Image.new('RGB', (width, height), color = (255, 255, 255))
+    
+    image_bg = Image.new('RGB', (width, height), color=(255, 255, 255))
     
     # set fonts
     font = ImageFont.truetype('./fonts/TiroTamil-Regular.ttf', font_size)
@@ -22,21 +22,25 @@ def draw_text(text, subtext):
 
     # create draw object
     draw = ImageDraw.Draw(image_bg)
-
+    
     # calculate text position to center it horizontally
-    text_width, text_height = draw.textsize(text, font=font)
+    text_bbox = draw.textbbox((0, 0), text, font=font)
+    text_width = text_bbox[2] - text_bbox[0]
+    text_height = text_bbox[3] - text_bbox[1]
     text_x = (width - text_width) / 2
     text_y = (height - text_height) / 2
-
+    
     # calculate subtext position to center it horizontally and put it below the main text
-    subtext_width, subtext_height = draw.textsize(subtext, font=subtext_font)
+    subtext_bbox = draw.textbbox((0, 0), subtext, font=subtext_font)
+    subtext_width = subtext_bbox[2] - subtext_bbox[0]
+    subtext_height = subtext_bbox[3] - subtext_bbox[1]
     subtext_x = (width - subtext_width) / 2
     subtext_y = text_y + text_height + subtext_font_size
-
+    
     # draw text
-    draw.text((text_x, text_y-100), text, font=font, align ="center", spacing=20, fill=(0, 0, 0))
-    draw.text((subtext_x, subtext_y+100), subtext, font=subtext_font, fill=(0, 0, 0))
-
+    draw.text((text_x, text_y - 100), text, font=font, align="center", spacing=20, fill=(0, 0, 0))
+    draw.text((subtext_x, subtext_y + 100), subtext, font=subtext_font, fill=(0, 0, 0))
+    
     return image_bg
 
 # test the function
